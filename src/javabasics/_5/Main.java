f3872b6bfeec9dc6b01c100e74c857d91ceeb7bd
Federico Scozzari <federicommscozzari@gmail.com>
2023-02-08 19:51:24 +0100

    Svolgimento ex.5

---
+++ src/javabasics/_5/Main.java
@@ -0,0 +1,129 @@
+package javabasics._5;
+
+public class Main { // Don't forget to use comments to clear things up if you need them!
+    public static void main(String[] args) {
+        exercise1();
+        exercise2();
+        exercise3();
+        exercise4Bonus();
+        exercise5Bonus();
+        playAroundWithModulus();
+    }
+
+    /*
+            Remember the symbols you need to do maths are
+
+            Plus = +
+            Minus = -
+            Multiply or times = *
+            Divide = /
+     */
+
+    /**
+     * 1: Create an int variable, add 1 to the value and then print it. Use the + operator
+     * Then minus 1 from the same variable and print it.
+     */
+    private static void exercise1() {
+        System.out.println("Exercise 1:");
+        // Write your code here
+        int age = 25;
+        System.out.println("my age next year : " + (age + 1));
+        System.out.println("my age last year : " + (age - 1));
+    }
+
+    /**
+     * 2: Create a double variable, times by 2 and then print it.
+     * <p>
+     * Create another double variable, which is the previous variable divided by 2
+     * and then print it
+     */
+    private static void exercise2() {
+        System.out.println("\nExercise 2:");
+        // Write your code here
+        double priceLunch = 8.99;
+        System.out.println("The price of lunch is : " + priceLunch * 2);
+        double priceBreakfast = priceLunch / 2;
+        System.out.println("The price of Breakfast is : " + priceBreakfast);
+    }
+
+    /**
+     * 3: We are going to convert the temperature from celsius to fahrenheit
+     * <p>
+     * Create a double variable called celsius
+     * <p>
+     * Create a new variable called fahrenheit
+     * <p>
+     * The variable fahrenheit should be set as
+     * celsius times by 1.8 plus 32
+     * <p>
+     * Print out celsius and fahrenheit with 2 print statements
+     */
+    private static void exercise3() {
+        System.out.println("\nExercise 3:");
+        // Write your code here
+        double celsius= 10.50;
+        double fahrenheit= celsius * 1.8 + 32;
+        System.out.println("Temperature celsius : " + celsius);
+        System.out.println("Temperature fahrenheit : " + fahrenheit);
+    }
+
+    /**
+     * 4: BONUS! This is an optional task, but it is recommended you complete it
+     * ----------------------------------------------------------------------
+     * Create an int called age with a starting value of 10.
+     * <p>
+     * Only using the '++' operator increase this value to 15
+     */
+    private static void exercise4Bonus() {
+        System.out.println("\nExercise 4:");
+        // Write your code here
+        int value = 10;
+        value++;
+        value++;
+        value++;
+        value++;
+        value++;
+        System.out.println("Value : " + value);
+    }
+
+    /**
+     * 5: BONUS! This is an optional task, but it is recommended you complete it
+     * ----------------------------------------------------------------------
+     * Create an int called age set as 50
+     * <p>
+     * Using the -= operator change this value from 50 to 0 in one line of code,
+     * <p>
+     * Underneath create another int called secondAge set as 50
+     * <p>
+     * Change this value from 50 to 0 in 3 lines of code
+     */
+    private static void exercise5Bonus() {
+        System.out.println("\nExercise 5:");
+        // Write your code here
+        int age = 50;
+        age -= 50;
+        System.out.println("Age : " + age);
+        int secondAge = 50;
+        secondAge -= 20;
+        secondAge -= 20;
+        secondAge -= 10;
+        System.out.println("Second age : " + secondAge);
+    }
+
+    /**
+     * This is completely optional, but if you guys want to play around with
+     * modulus to get a better understanding. Simply change the code below
+     */
+    private static void playAroundWithModulus() {
+        System.out.println("\nModulus testing!:");
+
+        int value = 10;   // <--- change this value
+        int modulus = 8; // <--- and this value to see the results of modulus
+
+        System.out.println("Modulus calculation, " + value + " % " + modulus +
+                "=" + value % modulus);
+    }
+
+}
+
+
