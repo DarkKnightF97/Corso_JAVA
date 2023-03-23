9dda5430441bc5f13bb79f9ac28a4d343e8574da
Federico Scozzari <federicommscozzari@gmail.com>
2023-03-23 02:42:09 +0100

    Svolgimento ex.oop.advanced.3

---
+++ src/javaoopadvanced/_3/Exercises.java
@@ -0,0 +1,55 @@
+package javaoopadvanced._3;
+
+public class Exercises {
+
+    public static void main(String[] args) {
+        exercise1();
+        exercise2();
+    }
+    /**
+     * 1:
+     *
+     *
+     * Create a class called "Person" with fields for name, age, and address.
+     *
+     * Override the `toString()` method in the Person class to display the object's name, age, and address in a user-friendly format.
+     *
+     * Create instances of the Person class and print them using the overridden `toString()` method.
+     */
+    private static void exercise1() {
+        System.out.println("\nExercise 1: ");
+        // Your code here
+        Person person = new Person("Federico", 25, "Via Zisa 24");
+        System.out.println(person.toString());
+    }
+
+    /**
+     * 2:
+     *
+     *
+     * Override the `hashCode()` method in the Person class to generate a unique hash code based on the object's name, age, and address.
+     *
+     * You can achieve this by using String.hashCode() to convert strings to ints and adding the fields together
+     *
+     * Create 2 people and print out their hashcodes
+     */
+    private static void exercise2() {
+        System.out.println("\nExercise 1: ");
+        // Your code here
+        Person person2 = new Person("Marco", 40,"Via Roma");
+        System.out.println("Hashcode person 2: "+ person2.hashCode());
+        Person person3 = new Person("Franco", 97, "Via Oreto");
+        System.out.println("Hashcode person 3: "+ person3.hashCode());
+    }
+
+    /**
+     * 3:
+     *
+     * BONUS: Read about the nuances of hashcodes here:
+     *
+     * https://www.baeldung.com/java-hashcode
+     *
+     * Don't necessarily expect to understand it all right away, it can get quite complicated
+     *
+     */
+}
